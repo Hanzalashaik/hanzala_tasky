434ee66217c70fa48839b8905a906ebf0d6a9304
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-08-30 12:22:04 +0000

    Hanzala's Tasky completed✅

--- app.js
+++ app.js
@@ -1,69 +1,73 @@
-import readline from "readline-sync"
+import readline from "readline-sync";
 import register from "./register.js";
 import login from "./login.js";
-import color from "cli-color"
+import color from "cli-color";
 
-export default async function main(){
-
-    try{
-
-
-        let c1=color.xterm(118)
-        let c2=color.xterm(7);
-        let c3=color.xterm(75);
-        let c4=color.xterm(218);
-        console.log(c1(
-   `    ----------------------------------------------------------------
+export default async function main() {
+  try {
+    let c1 = color.xterm(118);
+    let c2 = color.xterm(7);
+    let c3 = color.xterm(75);
+    let c4 = color.xterm(218);
+    console.log(
+      c1(
+        `    ----------------------------------------------------------------
     ---------------------WELCOME TO HANZALA'S TASKY-----------------
     -------------------------SELECT THE OPTIONS---------------------
-    ----------------------------------------------------------------`));
-    
-    let options=["For Exit","Register","Login"]
+    ----------------------------------------------------------------`
+      )
+    );
 
-    options.forEach((value,index)=>{
-        console.log(`${index+1}.${value}`);
-    })
+    let options = ["For Exit", "Register", "Login"];
 
-    let option=readline.question("Enter your choice: ")
+    options.forEach((value, index) => {
+      console.log(`${index + 1}.${value}`);
+    });
 
-    if(option< 0 || option>3 ||option==0){
-        console.log("Invalid Entry");
+    let option = readline.question("Enter your choice: ");
+
+    if (option < 0 || option > 3 || option == 0) {
+      console.log("Invalid Entry");
     }
 
+    switch (option) {
+      case "1":
+        console.clear();
+        console.log("Thank you visit again!!");
+        break;
 
-    switch(option){
-    case "1":
-        console.log("Thank you");
-        break
-        
-    case "2":
-        console.clear()
-        console.log(c1(
-       `        ----------------------------------------------------------------
+      case "2":
+        console.clear();
+        console.log(
+          c1(
+            `        ----------------------------------------------------------------
         --------------------------REGISTER HERE-------------------------
-        ----------------------------------------------------------------`));
-        
+        ----------------------------------------------------------------`
+          )
+        );
+
         await register();
         break;
 
-    case "3":
+      case "3":
         console.clear();
-        console.log(c1(
-           `        ----------------------------------------------------------------
+        console.log(
+          c1(
+            `        ----------------------------------------------------------------
             --------------------------LOGIN HERE----------------------------
-            ----------------------------------------------------------------`));
-        
+            ----------------------------------------------------------------`
+          )
+        );
+
         await login();
         break;
-        
-    default:
-        console.log(c3("Invalid Entry"));
-        break;        
-    }
-    }catch(e){
-        console.log("error from aap.js",e);
-        
-    }
-}
-main()
 
+      default:
+        console.log(c3("Invalid Entry"));
+        break;
+    }
+  } catch (e) {
+    console.log("error from aap.js", e);
+  }
+}
+main();
